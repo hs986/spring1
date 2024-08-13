7509217838aa073a3719690d283c1a315ef1bb50
hs986 <94986211+hs986@users.noreply.github.com>
2024-08-13 17:04:12 +0900

    컴포넌트 스캔과 자동 의존관계

---
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -0,0 +1,20 @@
+package hello.hello_spring.controller;
+
+
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    //private final MemberService memberService = new MemberService();
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }//오토와이어드 멤버서비스를  스프링이 컨데이너에 있는 멤버 서비스와 연결 (오류발생하는 이유 서비스에 @서비스가 안되서
+
+}
