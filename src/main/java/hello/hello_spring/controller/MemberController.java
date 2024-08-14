064d01bff78b150b42a2676f09d604cfe1bba58c
hs986 <94986211+hs986@users.noreply.github.com>
2024-08-14 14:03:16 +0900

    createMemberForm.html,membercontroller GetMapping

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -4,6 +4,7 @@ package hello.hello_spring.controller;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -17,4 +18,8 @@ public class MemberController {
         this.memberService = memberService;
     }//오토와이어드 멤버서비스를  스프링이 컨데이너에 있는 멤버 서비스와 연결 (오류발생하는 이유 서비스에 @서비스가 안되서
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
 }
